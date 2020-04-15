5267ac71a9f198fae74f77bfe75b2fecf6965522
hechusheng <601826520@qq.com>
2020-04-15 09:01:34 +0800

    Booktore

--- sc-provider/sc-xzsd-pc/src/main/java/com/xzsd/pc/menu/service/MenuService.java
+++ sc-provider/sc-xzsd-pc/src/main/java/com/xzsd/pc/menu/service/MenuService.java
@@ -6,7 +6,6 @@ import com.xzsd.pc.menu.dao.MenuDao;
 import com.xzsd.pc.menu.entity.MenuInfo;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import javax.annotation.Resource;
 import java.util.List;
 
